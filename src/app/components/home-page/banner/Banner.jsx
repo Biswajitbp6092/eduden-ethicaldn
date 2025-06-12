5778fe334012747beac8c81bb5f3bab364748ada
biswajitecd <biswajit@ethicalden.com>
2025-06-12 18:28:03 +0530

    banner section slick slider arrow

--- src/app/components/home-page/banner/Banner.jsx
+++ src/app/components/home-page/banner/Banner.jsx
@@ -1,48 +1,70 @@
-"use client";
-import React from "react";
-import Slider from "react-slick";
-import { GoArrowDownRight } from "react-icons/go";
-import { HiOutlineArrowNarrowDown } from "react-icons/hi";
+'use client';
+import React from 'react';
+import Slider from 'react-slick';
+import { GoArrowDownRight } from 'react-icons/go';
+import { HiOutlineArrowNarrowDown } from 'react-icons/hi';
+import { FaArrowLeftLong, FaArrowRightLong } from "react-icons/fa6";
+
+// Custom Arrows
+const NextArrow = ({ onClick }) => (
+  <div
+    className="absolute bottom-10 right-10 z-10 cursor-pointer text-white"
+    onClick={onClick}
+  >
+    <FaArrowRightLong />
+  </div>
+);
+
+const PrevArrow = ({ onClick }) => (
+  <div
+    className="absolute bottom-10 left-10 z-10 cursor-pointer text-white"
+    onClick={onClick}
+  >
+    <FaArrowLeftLong />
+  </div>
+);
 
 const Banner = () => {
   const bannerSetting = {
-    dots: false,
-    arow: false,
+    dots: true,
+    arrows: true,
     autoplay: true,
     autoplaySpeed: 3000,
     infinite: true,
     speed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
   };
 
   const bannerContent = [
     {
       id: 1,
-      toptitle: "WELCOME TO EDUDEN",
-      title: "Where Learning Meets Imagination & Innovation!",
+      toptitle: 'WELCOME TO EDUDEN',
+      title: 'Where Learning Meets Imagination & Innovation!',
       subtitle:
         "we're your one-stop destination for unlocking your potential and conquering the digital realm with confidence.",
-      button: "Discover More",
-      images: "/assets/banner-img.jpg",
+      button: 'Discover More',
+      images: '/assets/banner-img.jpg',
     },
     {
       id: 2,
-      toptitle: "WELCOME TO EDUDEN",
-      title: "Where Learning Meets Imagination & Innovation!",
+      toptitle: 'WELCOME TO EDUDEN',
+      title: 'Where Learning Meets Imagination & Innovation!',
       subtitle:
         "we're your one-stop destination for unlocking your potential and conquering the digital realm with confidence.",
-      button: "Discover More",
-      images: "/assets/banner-img.jpg",
+      button: 'Discover More',
+      images: '/assets/banner-img.jpg',
     },
     {
       id: 3,
-      toptitle: "WELCOME TO EDUDEN",
-      title: "Where Learning Meets Imagination & Innovation!",
+      toptitle: 'WELCOME TO EDUDEN',
+      title: 'Where Learning Meets Imagination & Innovation!',
       subtitle:
         "we're your one-stop destination for unlocking your potential and conquering the digital realm with confidence.",
-      button: "Discover More",
-      images: "/assets/banner-img.jpg",
+      button: 'Discover More',
+      images: '/assets/banner-img.jpg',
     },
   ];
 
@@ -64,7 +86,7 @@ const Banner = () => {
                   <p className="text-sm border border-white rounded-lg px-3 py-2 text-yellow-300 mb-4 inline-block">
                     {banner.toptitle}
                   </p>
-                  <h1 className=" text-[2rem] lg:text-6xl font-bold mb-4 max-w-xl leading-tight text-white">
+                  <h1 className="text-[2rem] lg:text-6xl font-bold mb-4 max-w-xl leading-tight text-white">
                     {banner.title}
                   </h1>
                   <p className="text-base max-w-xl mb-4 text-lg text-white">
@@ -77,9 +99,9 @@ const Banner = () => {
                 </div>
 
                 <div className="hidden md:flex w-[46px] h-[320px] bg-yellow-400 flex flex-col justify-between items-center rounded-full">
-                  <div className=" rotate-90 whitespace-nowrap pl-[6rem] font-bold">
+                  <div className="rotate-90 whitespace-nowrap pl-[6rem] font-bold">
                     follow now
-                  </div>                 
+                  </div>
 
                   <div className="flex flex-col items-center gap-2 py-2">
                     <HiOutlineArrowNarrowDown size={40} className="pb-4" />
@@ -90,7 +112,6 @@ const Banner = () => {
                         className="w-7 h-7 cursor-pointer transition duration-300 hover:scale-110 hover:drop-shadow-[0_0_5px_#000000]"
                       />
                     </a>
-
                     <a href="#" target="_blank" rel="">
                       <img
                         src="/assets/ig.svg"
@@ -98,7 +119,6 @@ const Banner = () => {
                         className="w-7 h-7 cursor-pointer transition duration-300 hover:scale-110 hover:drop-shadow-[0_0_5px_#000000]"
                       />
                     </a>
-
                     <a href="#" target="_blank" rel="">
                       <img
                         src="/assets/youtube.svg"
@@ -106,7 +126,6 @@ const Banner = () => {
                         className="w-7 h-7 cursor-pointer transition duration-300 hover:scale-110 hover:drop-shadow-[0_0_5px_#000000]"
                       />
                     </a>
-
                     <a href="#" target="_blank" rel="">
                       <img
                         src="/assets/in.svg"
@@ -120,7 +139,7 @@ const Banner = () => {
             </div>
           ))}
         </Slider>
-      </div>      
+      </div>
     </div>
   );
 };
